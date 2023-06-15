88cb4ae0c390fad1dcf8653ef0b013e763d740f7
Samson Tush <samo4421@icloud.com>
2023-06-15 16:41:43 +0300

    Add views

--- db/migrate/20230615132433_create_posts.rb
+++ db/migrate/20230615132433_create_posts.rb
@@ -1,11 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :author, null: false, foreign_key: true
+      t.belongs_to :user, foreign_key: true
       t.string :title
       t.text :text
 
       t.timestamps
     end
+    add_column :posts, :author_id, :integer
+    add_index :posts, :author_id
   end
 end
