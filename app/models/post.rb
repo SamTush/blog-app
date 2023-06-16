a911a5ba17a188fb1290e3e0db650f85b7c14fcc
Samson Tush <samo4421@icloud.com>
2023-06-16 12:54:41 +0300

    Add likes post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
+  has_many :likes
 
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
