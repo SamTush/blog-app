809aa680005620dc1c8326257ff8ca59a28ccf29
Samson Tush <samo4421@icloud.com>
2023-06-22 16:32:35 +0300

    Add route, actions and view files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id', counter_cache: true
   has_many :comments
   has_many :likes
 
