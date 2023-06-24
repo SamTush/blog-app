eaabc66541b8f6f259a5e5e92c2f71b3a0466bf5
Samson Tush <samo4421@icloud.com>
2023-06-24 18:09:43 +0300

    Fix path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index,:show] do 
-    resources :posts, only: [:index, :show]
-  
+  get 'likes/create'
+  get 'comments/new'
+  get 'comments/create'
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
+  post "users/:user_id/posts", to: "posts#create", as: :user_create_post
 end
